abb31177c37ec1ef62dd8425bb9553529eb2862a
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2020-07-26 11:13:42 -0300

    login screen finish

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,54 @@
 import React, { FormEvent } from 'react';
 import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
+import { toast } from 'react-toastify';
 
 import { } from 'react-icons/fa';
 
 import {
+  Container, WelcomeWrapper, WelcomeContent, AccessWrapper, AccessContent, AccessImage, AccessButton
 } from './styles';
+
+import logo from '../../assets/logo.jpg';
+import bg from '../../assets/bg.jpg';
 
 const Login = () => {
   const history = useHistory();
 
-  function handleSubmit(e: FormEvent) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
-    localStorage.setItem('token', '94a08da1fecbb6e8b46990538c7b50b2');
-
-    history.push('/');
+    try {
+      await api.get('isAlive');
+      localStorage.setItem('token', '94a08da1fecbb6e8b46990538c7b50b2');
+      history.push('/');
+    }
+    catch (error) {
+      console.log(error)
+      toast.error('Tente novamente mais tarde...');
+    }
   }
 
   return (
     <div id="page-login">
-      <button onClick={handleSubmit}>
-        Entrar
-      </button>
+      <Container>
+        <WelcomeWrapper>
+          <WelcomeContent backgroundImage={bg}>
+            <h1>Bem vindo</h1>
+            <p>Acesse o gerencial de carros</p>
+          </WelcomeContent>
+        </WelcomeWrapper>
+        <AccessWrapper>
+          <AccessContent>
+            <AccessImage>
+              <img src={logo} alt='logo' />
+            </AccessImage>
+            <AccessButton>
+              <button onClick={handleSubmit}>Entrar</button>
+            </AccessButton>
+          </AccessContent>
+        </AccessWrapper>
+      </Container>
     </div>
   )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,4 +1,139 @@
 import styled from 'styled-components';
 
+interface BackgroundImage {
+  backgroundImage: string;
+}
+
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  flex: 1 1 auto;
+  margin-right: -12px;
+  margin-left: -12px;
+  max-width: 100%;
+  overflow: hidden;
+`;
+
+export const WelcomeWrapper = styled.div`
+  padding: 0;
+  
+  @media (max-width: 960px) {
+    display: none;
+  }
+
+  @media (min-width: 960px) {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 33.3333333333%;
+    flex: 0 0 33.3333333333%;
+    max-width: 33.3333333333%;
+  }
+
+  @media (min-width: 1264px) {
+    flex: 0 0 58%;
+    max-width: 58%;
+  }
+`;
+
+export const WelcomeContent = styled.div<BackgroundImage>`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background: url(${props => props.backgroundImage}) no-repeat;
+  background-size: cover;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+
+  :before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(4, 57, 108, 0.9);
+  }
+
+  h1 {
+    color: #fff!important;
+    z-index: 999;
+    font-size: 50px;
+    border-bottom: .5px solid #fff;
+  }
+
+  p {
+    margin-top: 40px;
+    color: #fafafa!important;
+    opacity: .8;
+    font-size: 16px;
+    z-index: 999;
+  }
+`;
+
+export const AccessWrapper = styled.div`
+  @media (max-width: 960px) {
+    max-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  @media (min-width: 960px) {
+    flex: 0 0 33.3333333333%;
+    max-width: 33.3333333333%;
+  }
+
+  @media (min-width: 1264px) {
+    flex: 0 0 41.6666666667%;
+    max-width: 41.6666666667%;
+  }
+`;
+
+export const AccessContent = styled.div`
+  @media (min-width: 960px) {
+    max-width: 900px;
+  }
+
+  @media (min-width: 1264px) {
+    max-width: 1185px;
+  }
+
+  width: 100%;
+  padding: 12px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const AccessImage = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 80px;
+
+  img {
+    width: 150px;
+  }
+`;
+
+export const AccessButton = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button {
+    cursor: pointer;
+    margin-top: 16px;
+    border: none;
+    height: 36px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    padding: 0px 16px;
+    background-color: var(--blue-dark);
+    color: #fff;
+    font-size: 16px;
+    text-transform: uppercase;
+  }
 `;
